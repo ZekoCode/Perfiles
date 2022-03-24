e637022cb91ab30b24b110e851e31c60a4d47de8
ZekoCode <zekotheawesome@gmail.com>
2022-03-23 19:18:38 -0500

    Updated

---
+++ src/components/UserList.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useContext } from 'react';
+import UserContext from '../context/User/UserContext';
+
+const UserList = () => {
+
+  const { users, getUsers, getProfile } = useContext(UserContext)
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  return (
+    <div className="list-group h-100">
+      {
+        users.map(user => (
+          <a className='list-group-item list-group-item-action' href="#!" key={user.id} onClick={() => getProfile(user.id) }>
+            <img src={user.avatar} className="img-fluid mr-4 rounded-circle" width={70}/>
+            <p>
+              {`${user.first_name} ${user.last_name}`}
+            </p>
+          </a> 
+        ))
+      }
+    </div>
+  )
+}
+
+export default UserList;
